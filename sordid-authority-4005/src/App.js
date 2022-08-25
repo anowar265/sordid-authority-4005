3fbb54b7ea2e448dba7b2ff5d55da5e4b99a07ee
Ankur <ankurmishra710@gmail.com>
2022-08-25 18:52:40 +0530

    add svg

--- sordid-authority-4005/src/App.js
+++ sordid-authority-4005/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordions from './Accordions';
+import { Box } from '@chakra-ui/react';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. 
@@ -19,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
+      <Box backgroundColor="#9c3353;" width="100%" height="100px" color="white" fontSize="22px" fontWeight="400" padding='34px'><h2>Read what people are saying about Milaap!</h2></Box>
      <Accordions />
     </div>
   );
