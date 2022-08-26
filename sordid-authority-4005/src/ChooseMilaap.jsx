a3d3316cf996ef509f8a5d2c4f8f9436d65cb795
Ankur <ankurmishra710@gmail.com>
2022-08-26 16:19:25 +0530

    completed chose Milaap

--- sordid-authority-4005/src/ChooseMilaap.jsx
+++ sordid-authority-4005/src/ChooseMilaap.jsx
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
-import { Box, Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Image, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 const ChooseMilaap = () => {
   return (
-    <Box>
+    <Box p='4' bg="green.100">
         <Box fontSize="30px" fontFamily={"AvenirLTPro-Medium"} fontWeight="400">
 
         <h3  >Why choose Milaap?</h3>
         </Box>
-    <Flex>
-        <Box>
-            <Box bg="#fff">
-                <Box></Box>
-                <Box></Box>
+    <Flex  w="80%" marginLeft="200px" justifyContent="space-around">
+        <Box w="40%" maxH="800px">
+            <Box  h="150px" border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="5px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
             </Box>
-            <Box> <Image src={logo} className="App-logo" alt="logo" /></Box>
+            <br />
+            {/* //className="App-logo" */}
+            <Box marginLeft="-220px"marginTop="25%"  marginBottom={"-5"}> <Image w="59%" h="300px" src={logo}  alt="logo" />
+            </Box>
+           
         </Box>
-        <Box></Box>
-        <Box></Box>
+        <Spacer />
+        <Box width="25%"  borderColor="#e0e1e3">
+        <Box h="150px" border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="10px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
+            </Box>
+            <br />
+            
+            <Box h="150px"  border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="5px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
+            </Box>
+            <br />
+            <Box h="150px"  border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="5px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
+            </Box>
+        </Box>
+        <Spacer />
+        <Box width="25%"  borderColor="#e0e1e3">
+        <Box h="150px" border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="10px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
+            </Box>
+            <br />
+            
+            <Box h="150px"  border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="5px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
+            </Box>
+            <br />
+            <Box h="150px"  border="2px dashed" bg="#fff" borderColor="#e0e1e3" borderRadius="5px">
+                <Box  borderBottom="2px dashed" bg="#fff" borderColor="#e0e1e3">ii</Box>
+                <Box>ooo</Box>
+            </Box>
+        </Box>
     </Flex>
     </Box>
   )
